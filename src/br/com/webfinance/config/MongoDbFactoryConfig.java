4a3617499422dd0f4ce25cfbbabb738e1408a969
shyba <victor.shyba@gmail.com>
2014-03-24 22:30:33 -0300

    moving old files to old/ and adding a README.md

--- src/br/com/webfinance/config/MongoDbFactoryConfig.java
+++
@@ -1,9 +0,0 @@
-package br.com.webfinance.config;
-
-import org.springframework.data.mongodb.MongoDbFactory;
-
-public interface MongoDbFactoryConfig {
-
-	public abstract MongoDbFactory mongoDbFactory() throws Exception;
-
-}
--- src/br/com/webfinance/repo/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.webfinance.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import br.com.webfinance.model.Role;
-
-
-public interface RoleRepository extends MongoRepository<Role, String> {
-}
