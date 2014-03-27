3811eda649c66527d4c93ba358bdddd1f1f558d1
shyba <victor.shyba@gmail.com>
2014-03-27 00:33:04 -0300

    adding pymongo and updating scaffolded test, closes #2

--- mongofinance/tests.py
+++ mongofinance/tests.py
@@ -1,17 +1,18 @@
 import unittest
 
 from pyramid import testing
-
+import pymongo
 
 class ViewTests(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
+        self.request = testing.DummyRequest()
+        self.request.db = pymongo.Connection()['testing_db']
 
     def tearDown(self):
         testing.tearDown()
 
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'MongoFinance')
+    def test_home_view(self):
+        from .views import home_view
+        info = home_view(self.request)
+        self.assertEquals(info,{})
