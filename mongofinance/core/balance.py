e9ad11a324e1312141013242d11df11546a8ed53
shyba <victor.shyba@gmail.com>
2014-04-12 03:25:42 -0300

    Balance class, closes issue #6

---
+++ mongofinance/core/balance.py
@@ -0,0 +1,17 @@
+"""
+    Balance logic module.
+"""
+
+class Balance():
+    """
+        Represents a bank balance
+    """
+    
+    def __init__(self, current=0, initial=0):
+        """
+            Initializes the balance representation
+        """
+        self.current=current
+        self.initial=initial
+
+
