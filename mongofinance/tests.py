85d426c398edcf597834285a8427fda1055cf296
shyba <victor.shyba@gmail.com>
2014-04-12 03:25:10 -0300

    tests for issue #6

--- mongofinance/tests.py
+++ mongofinance/tests.py
@@ -1,5 +1,5 @@
 import unittest
-
+from core.balance import Balance
 from pyramid import testing
 import pymongo
 
@@ -16,3 +16,26 @@ class ViewTests(unittest.TestCase):
         from .views import home_view
         info = home_view(self.request)
         self.assertEquals(info,{})
+
+
+
+class TestBalance(unittest.TestCase):
+    def testUpdateCurrentBalance(self):
+        """
+            1:Update and view the current bank balance
+        """
+        balance = Balance(current=100)
+        self.assertEquals(balance.current, 100)
+        self.assertEquals(balance.initial, 0)
+        balance.current = 50
+        self.assertEquals(balance.current, 50)
+
+    def testInitialBalance(self):
+        """
+            2:Set the initial balance as a fixed value (like your salary)
+        """
+        balance = Balance(initial=1000)
+        self.assertEquals(balance.initial, 1000)
+
+
+
