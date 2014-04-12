1bede71f7fdf6723917693f1d95d8372eccfd806
shyba <victor.shyba@gmail.com>
2014-04-12 03:52:42 -0300

    PEP8

--- mongofinance/core/balance.py
+++ mongofinance/core/balance.py
@@ -2,16 +2,16 @@
     Balance logic module.
 """
 
+
 class Balance():
+
     """
         Represents a bank balance
     """
-    
+
     def __init__(self, current=0, initial=0):
         """
             Initializes the balance representation
         """
-        self.current=current
-        self.initial=initial
-
-
+        self.current = current
+        self.initial = initial
