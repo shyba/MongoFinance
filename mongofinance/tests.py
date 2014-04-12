82ac99269b1c140f13ccc20a66e2042d918c6510
shyba <victor.shyba@gmail.com>
2014-04-12 03:52:07 -0300

    PEP8 + tests for expense class from issue #5

--- mongofinance/tests.py
+++ mongofinance/tests.py
@@ -1,9 +1,16 @@
+"""
+    Main test file. Should be splitted soon.
+"""
 import unittest
-from core.balance import Balance
+from mongofinance.core.balance import Balance
+from mongofinance.core.expense import Expense
 from pyramid import testing
 import pymongo
+import datetime
+
 
 class ViewTests(unittest.TestCase):
+
     def setUp(self):
         self.config = testing.setUp()
         self.request = testing.DummyRequest()
@@ -13,14 +20,17 @@ class ViewTests(unittest.TestCase):
         testing.tearDown()
 
     def test_home_view(self):
+        """
+        Tests the / route
+        """
         from .views import home_view
         info = home_view(self.request)
-        self.assertEquals(info,{})
-
+        self.assertEquals(info, {})
 
 
 class TestBalance(unittest.TestCase):
-    def testUpdateCurrentBalance(self):
+
+    def test_update_current_balance(self):
         """
             1:Update and view the current bank balance
         """
@@ -30,7 +40,7 @@ class TestBalance(unittest.TestCase):
         balance.current = 50
         self.assertEquals(balance.current, 50)
 
-    def testInitialBalance(self):
+    def test_initial_balance(self):
         """
             2:Set the initial balance as a fixed value (like your salary)
         """
@@ -38,4 +48,15 @@ class TestBalance(unittest.TestCase):
         self.assertEquals(balance.initial, 1000)
 
 
+class TestExpenses(unittest.TestCase):
 
+    def test_create_default_expense(self):
+        """
+            1:The expense will be added as unpaid.
+            2:The expense will have an optional payment date. 
+            When not specified, the current month will be used.
+        """
+        expense = Expense("Dinner", 100)
+        self.assertFalse(expense.is_paid)
+        today = datetime.date.today()
+        self.assertEqual(expense.date, today)
