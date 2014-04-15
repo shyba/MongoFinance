4901721c47a290b31b12f10dc737cd7261d98df7
shyba <victor.shyba@gmail.com>
2014-04-15 04:25:21 -0300

    adding mocked RecurringExpense class

--- mongofinance/core/expense.py
+++ mongofinance/core/expense.py
@@ -4,10 +4,32 @@
 import datetime
 
 
-class Expense():
+class Expense(object):
+    """
+    This class represents an Expense
+    """
 
-    def __init__(self, name, value):
+    def __init__(self, name, value, date=None):
         self.name = name
         self.value = value
-        self.date = datetime.date.today()
+        if not date:
+            self.date = datetime.date.today()
+        else:
+            self.date = date
         self.is_paid = False
+
+class RecurringExpense(object):
+    """
+    Expense generator based on interval and dates
+    """
+
+    def __init__(self, expense, interval_days, end_date=None):
+        self.end_date = end_date
+        self.expense = expense
+        self.interval = interval_days
+
+    def generate(self):
+        """
+        Returns a list with generated expenses
+        """
+        return [self.expense]
