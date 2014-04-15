219bbaf88879842c2d666f19a7dc4326a2371daf
shyba <victor.shyba@gmail.com>
2014-04-15 04:24:54 -0300

    adding recurrent test

--- mongofinance/tests.py
+++ mongofinance/tests.py
@@ -4,12 +4,16 @@
 import unittest
 from mongofinance.core.balance import Balance
 from mongofinance.core.expense import Expense
+from mongofinance.core.expense import RecurringExpense
 from pyramid import testing
 import pymongo
 import datetime
 
 
 class ViewTests(unittest.TestCase):
+
+    """ Generic tests for all the views
+    """
 
     def setUp(self):
         self.config = testing.setUp()
@@ -23,12 +27,16 @@ class ViewTests(unittest.TestCase):
         """
         Tests the / route
         """
-        from .views import home_view
+        from mongofinance.views import home_view
         info = home_view(self.request)
         self.assertEquals(info, {})
 
 
 class TestBalance(unittest.TestCase):
+
+    """
+    Bank balance tests
+    """
 
     def test_update_current_balance(self):
         """
@@ -50,13 +58,28 @@ class TestBalance(unittest.TestCase):
 
 class TestExpenses(unittest.TestCase):
 
+    """
+    Expenses tests
+    """
+
     def test_create_default_expense(self):
         """
             1:The expense will be added as unpaid.
-            2:The expense will have an optional payment date. 
+            2:The expense will have an optional payment date.
             When not specified, the current month will be used.
         """
         expense = Expense("Dinner", 100)
         self.assertFalse(expense.is_paid)
         today = datetime.date.today()
         self.assertEqual(expense.date, today)
+
+    def test_recurrent_expenses(self):
+        """
+        Generate expenses based on a recurring expense
+        """
+        expense = Expense("Car Wash", value=20, date="01-04-2014")
+        recurring = RecurringExpense(
+            expense, interval_days=20, end_date="21-04-2014")
+        generated = recurring.generate()
+        self.assertEquals(1, len(generated))
+        self.assertEquals(expense, generated[0])
